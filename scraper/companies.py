f129f76b4afd0d4fac879c896d3302429793e782
Quentin Mowat-Amiet <quentinmowat-amiet@d0002332.iese.org>
2024-03-28 16:24:24 +0100

    feat(creat_urls): Add create_urls func
    Add function that builds URLs from company name and id found in Company table.

--- scraper/companies.py
+++ scraper/companies.py
@@ -1,17 +1,22 @@
-from requests.exceptions import RequestException
+from requests import RequestException, HTTPError, ConnectionError, Timeout, TooManyRedirects
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select
 from urllib.parse import quote
+import urllib3
 import simplejson as json
 import requests
 
 from typing import Dict
 from dotenv import load_dotenv
 from time import process_time
+import time
 import asyncio
 import os
 import sys
 
+# Disable SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Load .env file
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -19,63 +24,108 @@ load_dotenv()
 
 from database import Company, get_db
 from log import logger, setup_logging
+from utils import Url
 
 
 async def find_company(query: str) -> Dict[str, str | int]:
+    """
+    Search a company on Glassdoor based on the given query (company name).
 
-    global rate_limit_exceeded
+    Args:
+        query (str): The company name to search for.
 
-    # You will have to change the request format based on the proxy service used
+    Returns:
+        Dict[str, str | int]: A dictionary containing the employer ID and name of the best match.
+
+    Raises:
+        HTTPError: If there is an HTTP error during the request.
+        ConnectionError: If there is a network connection error.
+        Timeout: If the request times out.
+        TooManyRedirects: If there are too many redirects during the request.
+        RequestException: If there is any other request error.
+
+    Note:
+        This function requires the following environment variables to be set:
+        - SMART_USERNAME: The username for the smartproxy service.
+        - SMART_PASSWORD: The password for the smartproxy service.
+        or 
+        - OXYLABS_USERNAME: The username for the oxylabs service.
+        - OXYLABS_PASSWORD: The password for the oxylabs service.
+        or
+        another serivce you are using.
+
+        Depending on the proxy service you use, you will have to change the proxy and headers objects,
+        as well as the request format.
+    """
+    # Global variables was used for Oxylab's free trial 
+    # global rate_limit_exceeded
+
+    # You will have to change the proxies and headers objects based on the proxy service you use
     # Proxy crendetials
     cred = {
-        "username": os.getenv("OXY_USERNAME"),
-        "password": os.getenv("OXY_PASSWORD"),
+        "username": os.getenv("SMART_USERNAME"),
+        "password": os.getenv("SMART_PASSWORD"),
     }
-
-    # Structure payload.
-    payload = {
-        "source": "universal",
-        "url": f"https://www.glassdoor.com/searchsuggest/typeahead?numSuggestions=8&source=GD_V2&version=NEW&rf=full&fallback=token&input={quote(query)}",
+    proxies = {
+        "http": f'http://{cred["username"]}:{cred["password"]}@unblock.smartproxy.com:60000',
+        "https": f'http://{cred["username"]}:{cred["password"]}@unblock.smartproxy.com:60000',
     }
+    headers = {"X-SU-Headles": "html"}
 
-    try:
-        # Get response.
-        response = requests.request(
-            "POST",
-            "https://realtime.oxylabs.io/v1/queries",
-            auth=(cred["username"], cred["password"]),
-            json=payload,
-        )
+    # Search URL with query replaced by the company name
+    url = f"https://www.glassdoor.com/searchsuggest/typeahead?numSuggestions=8&source=GD_V2&version=NEW&rf=full&fallback=token&input={quote(query)}"
+    
+    http_attempts, max_http_attempts = 0, 10
+    network_attempts, max_network_attempts = 0, 60
+    while http_attempts < max_http_attempts and network_attempts < max_network_attempts:
+        response = None
+        try:
+            response = requests.request(
+                "GET", url, verify=False, proxies=proxies, headers=headers
+            )
+            response.raise_for_status()
+            break
 
-        ########################### TESTING ###########################
-        # Check response format, will changed based on proxy service used
-        # print(f"\nRequest URL: {response.url}\nPayload: {payload}")
-        # print(
-        #     f"\nResponse status: {response.status_code}\nHeaders: {response.headers}\nBody: {response.text}"
-        # )
+            # For oxylabs free trial rate limit
+            # # Check if the status code is 429
+            # if response.status_code == 429:
+            #     logger.error(f"Exceeded proxy network rate limit: {response.status_code}")
+            #     rate_limit_exceeded = True  # Set the flag
+            #     return  # Stop the task
 
-        # Check if the status code is 429
-        if response.status_code == 429:
-            logger.error(f"Exceeded proxy network rate limit: {response.status_code}")
-            rate_limit_exceeded = True  # Set the flag
-            return  # Stop the task
+            # # Check if the status code is not 200
+            # if response.status_code != 200:
+            #     logger.error(f"Unexpected status code: {response.status_code}")
+            #     return  # Stop the task
 
-        # Check if the status code is not 200
-        if response.status_code != 200:
-            logger.error(f"Unexpected status code: {response.status_code}")
-            return  # Stop the task
-
-    except RequestException as e:
-        raise Exception(f"Request error to Glassdoor: {e}")
+        except HTTPError as e:
+            status_code = response.status_code if response else 'No response code'
+            logger.error(f'HTTP error: {e}', extra={"status_code": status_code, "URL": url})
+            http_attempts += 1
+            time.sleep(1)
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            status_code = response.status_code if response else 'No response code'
+            logger.error(f'Network error: {e}', extra={"status_code": status_code, "URL": url})
+            network_attempts += 1
+            time.sleep(30)
+        except RequestException as e:
+            status_code = response.status_code if response else 'No response code'
+            logger.error(f'Other request error: {e}', extra={"status_code": status_code, "URL": url})
+            return None
+    
+    ########################### TESTING ###########################
+    # Uncomment to check response format, this will changed based on the proxy service used
+    # print(f"\nRequest URL: {response.url}")
+    # print(
+    #     f"\nResponse status: {response.status_code}\n\nHeaders: {response.headers}\n\nBody: {response.text}\n"
+    # )
 
     # Load the JSON string into dictionary
-    response_json = json.loads(response.text)
-
-    # TESTING
-    # print(f"\nResponse JSON:\n{response_json}\n")
+    # response_json = json.loads(response.text)  # oxylabs
+    suggestions = json.loads(response.text)
 
     # Load content from the response
-    suggestions = json.loads(response_json["results"][0]["content"])
+    # suggestions = json.loads(response_json["results"][0]["content"])   # oxylabs
 
     # Filter the suggestions to only include those with category as company or multicat
     company_suggestions = [
@@ -88,11 +138,24 @@ async def find_company(query: str) -> Dict[str, str | int]:
     if not company_suggestions:
         logger.error(f"No search results on Glassdoor: {query}")
         return None
+    
+    # Check if there's a direct hit in the suggestions
+    direct_hit_suggestions = [
+        suggestion
+        for suggestion in company_suggestions
+        if suggestion["directHit"] == True
+    ]
 
-    # Select the suggestion with the highest confidence value
-    best_match = max(
-        company_suggestions, key=lambda suggestion: suggestion["confidence"]
-    )
+    # Check if there was a direct hit before taking search suggestion with max confidence 
+    if direct_hit_suggestions:
+        best_match = max(
+            direct_hit_suggestions, key=lambda suggestion: suggestion["confidence"]
+        )
+    else:
+        # Select the suggestion with the highest confidence value
+        best_match = max(
+            company_suggestions, key=lambda suggestion: suggestion["confidence"]
+        )
 
     # Check if employer_name is None or an empty string
     if not best_match["suggestion"]:
@@ -106,11 +169,22 @@ async def find_company(query: str) -> Dict[str, str | int]:
 
 
 async def add_company_to_db(company_name: str, db: Session, lock: asyncio.Lock) -> None:
+    """
+    Add a company anme and id to the database.
 
-    global rate_limit_exceeded
-    # Check the flag before sending a request
-    if rate_limit_exceeded:
-        return
+    Args:
+        company_name (str): The name of the company to add.
+        db (Session): The database session.
+        lock (asyncio.Lock): The lock used for synchronization.
+
+    Returns:
+        None
+    """
+    # Global variables was used for Oxylab's free trial 
+    # global rate_limit_exceeded
+    # # Check the flag before sending a request
+    # if rate_limit_exceeded:
+    #     return
 
     async with lock:
         company_data = await find_company(company_name)
@@ -149,17 +223,38 @@ async def add_company_to_db(company_name: str, db: Session, lock: asyncio.Lock) 
 
 
 async def find_all_companies(db: Session, lock: asyncio.Lock) -> Dict[str, str]:
+    """
+    Find all companies in the database and add them to the database with error handling.
 
+    Args:
+        db (Session): The database session.
+        lock (asyncio.Lock): The lock used for synchronization.
+
+    Returns:
+        Dict[str, str]: A dictionary with a message indicating the completion of the scraping process.
+    """
     # Query the Company table
     companies = db.query(Company).all()
 
     async def add_company_with_error_handling(
-        company: Company, db: Session, lock: asyncio.Lock
-    ) -> None:
-        try:
-            await add_company_to_db(company.employer_name, db, lock)
-        except Exception as e:
-            logger.error(f"Error adding to database: {company.employer_name}, {e}")
+            company: Company, db: Session, lock: asyncio.Lock
+        ) -> None:
+            """
+            Adds a company to the database with error handling.
+
+            Args:
+                company (Company): The company object to add to the database.
+                db (Session): The database session.
+                lock (asyncio.Lock): The lock to synchronize access to the database.
+
+            Raises:
+                Exception: If there is an error adding the company to the database.
+
+            """
+            try:
+                await add_company_to_db(company.employer_name, db, lock)
+            except Exception as e:
+                logger.error(f"Error adding to database: {company.employer_name}, {e}")
 
     tasks = []
     for company in companies:
@@ -173,22 +268,62 @@ async def find_all_companies(db: Session, lock: asyncio.Lock) -> Dict[str, str]:
     return {"message": "Scraping complete, employer id added to the database"}
 
 
+def create_urls(db: Session) -> None:
+    """
+    Update the `url_new` field for each company in the Company table.
+
+    Args:
+        db (Session): The database session.
+        lock (asyncio.Lock): The lock used for synchronization.
+
+    Returns:
+        None
+    """
+    # Query the Company table for only employer_id, employer_name and url_new
+    stmt = select(Company.employer_id, Company.employer_name, Company.url_new)
+    companies = db.execute(stmt).fetchall()
+
+    for company in companies:
+        employer_id, employer_name, _url_new = company
+
+        # Build the URL with the Url.reviews method
+        url_new = Url.reviews(employer_name, employer_id)
+
+        # Update the url_new field with the new URL
+        company.url_new = url_new
+
+    # Commit the changes to the database
+    db.commit()
+
+
+
 if __name__ == "__main__":
+    # Test response, if uncomment the code below, you will have to comment the code below the test_dict
+    # test_dict = asyncio.run(find_company("NVIDIA")) # Meta id: 40772, Nvidia id: 7633, Google id: 9079
+    # print(f"\nTest response: {test_dict}\n")
+
     # Configure logging
     listener = setup_logging()
 
     # Create a lock to be used in the async function
     lock = asyncio.Lock()
 
+    # For oxlabs free trial rate limit
     # Initialize the rate_limit_exceeded flag
-    rate_limit_exceeded = False
+    # rate_limit_exceeded = False
 
     # Start time
     start_time = process_time()
 
     # Get a database session
     with get_db() as db:
+        # Run find_all_companies to search for all company names in employer_name field in Company table
         asyncio.run(find_all_companies(db, lock))
+
+    # Get a database session
+    with get_db() as db:
+        # Run create_urls to create the URLs for each company from employer_if and employer_name fields in Company table
+        asyncio.run(create_urls(db))
 
     # End time
     end_time = process_time()
@@ -198,3 +333,5 @@ if __name__ == "__main__":
 
     # # Stop the listener
     listener.stop()
+
+    
